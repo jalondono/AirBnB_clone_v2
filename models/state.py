b74f74066456ea16f3f286628830974d2c78cca2
jalondono <juanlondono151776@hotmail.com>
2019-12-18 10:33:38 -0500

    * added relationship with filestorage

--- models/state.py
+++ models/state.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """This is the state class"""
 import os
+
+from models import City
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import models
 
 
 class State(BaseModel, Base):
@@ -15,3 +18,13 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
         cities = relationship("City", backref="state", cascade="delete")
+    else:
+        @property
+        def cities(self):
+            '''FileStorage relationship between State and City '''
+            cities = models.storage.all(City)
+            cities_relation = []
+            for city in cities.values():
+                if city.state_id == self.id:
+                    cities_relation = cities_relation.append(city)
+            return cities_relation
