cd4aa103988fd26bac4c446ece73f4c942f70631
jalondono <juanlondono151776@hotmail.com>
2020-01-21 16:31:50 -0500

    * Upload task 5 again

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,56 +1,44 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application"""
+"""
+starts a Flask web application
+"""
 from flask import Flask
 from flask import render_template
-
 app = Flask(__name__)
 
-app.route('/')
 
-
-@app.route("/", strict_slashes=False)
-def print():
-    """Return a string"""
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
     return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
-def print_2():
-    """Return a string"""
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
     return 'HBNB'
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def print_c(text):
-    """print  a directory"""
-    aux_text = text.replace('_', ' ')
-    return "C {}".format(aux_text)
+@app.route('/c/<text>', strict_slashes=False)
+def show_c(text):
+    # display “C ” followed by the value of the text
+    return 'C %s' % text.replace("_", " ")
 
 
-@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route("/python/<text>")
-def print_python(text):
-    """display “Python ”, followed by the value
-    of the text variable
-    (replace underscore _ symbols with a space"""
-    aux_text = text.replace('_', ' ')
-    return "Python {}".format(aux_text)
+@app.route('/python/', strict_slashes=False, defaults={"text": "is cool"})
+@app.route('/python/<text>')
+def show_python(text):
+    # display “Python ”, followed by the value of the text
+    return 'Python %s' % text.replace("_", " ")
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
-def print_number(n):
-    """display “Python ”, followed by the value
-    of the text variable
-    (replace underscore _ symbols with a space"""
-    if type(n) is int:
-        return "{:d} is a number".format(n)
+@app.route('/number/<int:n>', strict_slashes=False)
+def show_number(n):
+    # display “n is a number” only if n is an integer
+    return '%d is a number' % n
 
 
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def insert_num_html(n):
-    """display “Python ”, followed by the value
-    of the text variable
-    (replace underscore _ symbols with a space"""
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n=None):
+    # display “n is a number” template
     return render_template('5-number.html', n=n)
 
 
