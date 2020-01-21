eb9d231202a878b157d353e43b6dd440e0564bcd
jalondono <juanlondono151776@hotmail.com>
2020-01-21 15:57:07 -0500

    * 7-states_list.html Added the template code to show the list of states * 7-states_list.py Added teardown method to close the session * Added states_list method to render the template with the classes

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,9 +1,30 @@
-# !/usr/bin/python3
-"""script that starts a Flask web application"""
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+import re
 from flask import Flask
 from flask import render_template
 from models import storage
+from models.state import State
+from models.city import City
+from collections import OrderedDict
+
+app = Flask(__name__)
 
 
-print("oli")
-storage()
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
+@app.route('/states_list/', strict_slashes=False)
+def states_list():
+    all_states = storage.all('State')
+    print(all_states)
+    return render_template('7-states_list.html', states=all_states)
+
+
+if __name__ == '__main__':
+    app.run()
